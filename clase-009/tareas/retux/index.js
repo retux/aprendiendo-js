44d870ccad157bb2a7590142f0b43aa02cb6d721
Matias Gutierrez Reto <Matias.Gutierrez.Reto@ey.com>
2020-07-10 00:11:40 -0300

    Primera versión que funciona. Algunas mejoras vendrán.

--- clase-009/tareas/retux/index.js
+++ clase-009/tareas/retux/index.js
@@ -1,5 +1,10 @@
+// chusmita: aplicación que te hace preguntas basándose en un template de entrada (./input/preguntas.tpl).
+//  Los datos son escritos en ./var/chusmita.json y posteriormente pueden editarse con el menú.
+// autor: retux
+
 const readline = require('readline')
 const path = require('path')
+const os = require('os')
 const common = require('./lib/common')
 
 const rl = readline.createInterface({
@@ -15,9 +20,7 @@ const prompt = async (pregunta) => {
 
 const crearDirsinoExiste = async (dirPath) => {
   try {
-    if (await common.archivoExiste('./var')) {
-      console.log('[info] directorio var existe.')
-    }
+    await common.archivoExiste('./var')
   } catch (error) {
     if (error.code === 'ENOENT') {
       console.log(`[info] el directorio ${dirPath} no existe. Creándolo...`)
@@ -31,23 +34,28 @@ const crearDirsinoExiste = async (dirPath) => {
 const leerTemplate = async (filePath) => {
   let listadePreguntas
   try {
-    if (await common.archivoExiste(filePath)) {
-      console.log('[info] Si si si, archivo .tpl existe!!')
-    }
+    // if (await common.archivoExiste(filePath)) {
+    //   console.log('[info] Si si si, archivo .tpl existe!!')
+    // }
     const template = await common.leerArchivo(filePath)
     listadePreguntas = JSON.parse(template)
   } catch (error) {
     console.log(`[error] No pude abrir el archivo preguntas.tpl o parsear el json: ${error.message}`)
   }
-  //console.log(listadePreguntas)
   return listadePreguntas
 }
 
 const recorrerPreguntas = async (listadePreguntas) => {
+  let valor
   const toReturn = {}
-  const entradas = Object.entries(listadePreguntas)
+  const entradas = Object.keys(listadePreguntas)
   for (let i = 0; i < entradas.length; i++) {
-    toReturn[entradas[i][0]] = await prompt(`${entradas[i][0]} `)
+    valor = await prompt(entradas[i] + ' [' + listadePreguntas[entradas[i]] + '] ')
+    if (valor.length > 0) {
+      toReturn[entradas[i]] = valor
+    } else {
+      toReturn[entradas[i]] = listadePreguntas[entradas[i]]
+    }
   }
   return toReturn
 }
@@ -60,24 +68,67 @@ const guardarDatos = async (filePath, datos) => {
   }
 }
 
+const editarDatos = async (archivodeDatos) => {
+  const listadePreguntas = await leerTemplate(archivodeDatos)
+  const objSalida = await recorrerPreguntas(listadePreguntas)
+  await guardarDatos(archivodeDatos, JSON.stringify(objSalida, null, 2))
+  console.log('done con editar datos.')
+}
+
+const imprimirAyuda = _ => {
+  console.log(os.EOL + 'Opciones: ' + os.EOL)
+  console.log('h  Imprime esta ayuda.')
+  console.log('q  salir del programa.')
+  console.log('e  editar datos del archivo.')
+  console.log('p  imprimir datos de archivo.')
+}
+
+const imprimirDatos = async (filePath) => {
+  const listadePreguntas = await leerTemplate(filePath)
+  const entradas = Object.keys(listadePreguntas)
+  console.log('')
+  for (let i = 0; i < entradas.length; i++) {
+    console.log(`${entradas[i]}: ${listadePreguntas[entradas[i]]}`)
+  }
+}
+
+const salir = _ => {
+  rl.close()
+  process.exit(0)
+}
+
 (async _ => {
-  const ps1 = '.:chusmita:. >'
+  console.clear()
+  const ps1 = os.EOL + '.:chusmita:. (h para ayuda) > '
   let listadePreguntas
   const archivodeDatos = path.join(__dirname, 'var', 'chusmita.json')
   await crearDirsinoExiste(path.join(__dirname, 'var'))
+  // Si el archivo ./var/chusmita.json no existe empezará haciendo las preguntas del template.
   try {
     await common.archivoExiste(path.join(archivodeDatos))
   } catch {
-    console.log('[debug] archivo var/chusmita.json no existe. Procesando preguntas del template.')
-    console.log('[info] Voy a hacerte algunas preguntas')
+    console.log('[info] Voy a empezar haciéndote algunas preguntas.' + os.EOL)
     listadePreguntas = await leerTemplate(path.join(__dirname, 'input', 'preguntas.tpl'))
     const objSalida = await recorrerPreguntas(listadePreguntas)
-    guardarDatos(archivodeDatos, JSON.stringify(objSalida, null, 2))
-    console.log(objSalida)
+    await guardarDatos(archivodeDatos, JSON.stringify(objSalida, null, 2))
   }
-  //
-  //
-  const nombre = await prompt(ps1)
-  console.log(`Hola ${nombre}`)
-  rl.close()
+  while (true) {
+    const comando = await prompt(ps1)
+    switch (comando) {
+      case 'h':
+        imprimirAyuda()
+        break
+      case 'q':
+        salir()
+        break
+      case 'e':
+        await editarDatos(archivodeDatos)
+        break
+      case 'p':
+        await imprimirDatos(archivodeDatos)
+        break
+      default:
+        console.log('Opción inválida. Reintentá ameo por favor.')
+    }
+  }
 })()
